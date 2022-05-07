57237c6fc0bce0734e28a93d84d18d503aa4cabc
Barry Mullan <bmullan@vmware.com>
2022-05-07 16:06:26 -0400

    formatting updates

--- MBTAStreamingClient/src/main/java/org/cscie88/MBTAStreamingClient/HttpStreamingClient.java
+++ MBTAStreamingClient/src/main/java/org/cscie88/MBTAStreamingClient/HttpStreamingClient.java
@@ -26,92 +26,90 @@ public class HttpStreamingClient {
 
     private static final Logger logger = LoggerFactory.getLogger(MbtaStreamingClientApplication.class);
 
-	// these value can be overridden with the corresponding environment variables
-	@Value("${MBTA_API_ENDPOINT:https://api-v3.mbta.com}")
+    // these value can be overridden with the corresponding environment variables
+    @Value("${MBTA_API_ENDPOINT:https://api-v3.mbta.com}")
     String mbtaApiEndpoint;
-	@Value("${MBTA_API_KEY:948fdf59bd114be9b9af1bcfea2d93ad}")
-	String mbtaApiKey;
-	@Value("${MBTA_API_URI:/vehicles}")
-	String mbtaApiUri; 
-	@Value("${MBTA_KAFKA_VEHICLE_TOPIC:cscie88_mbta_vehicles}")
+    @Value("${MBTA_API_KEY}")
+    String mbtaApiKey;
+    @Value("${MBTA_API_URI:/vehicles}")
+    String mbtaApiUri;
+    @Value("${MBTA_KAFKA_VEHICLE_TOPIC:cscie88_mbta_vehicles}")
     String mbtaTopicName;
 
-
     @Autowired
-	KafkaTemplate template;
+    KafkaTemplate template;
 
-	// this method in this class will be called (by the spring framework) once the class is constructed
+    // this method in this class will be called (by the spring framework) once the class is constructed
     @PostConstruct
     public void run() {
 
-			// reference kafka headers
-			// https://www.confluent.io/blog/5-things-every-kafka-developer-should-know/#tip-5-power-of-record-headers
+        // reference kafka headers
+        // https://www.confluent.io/blog/5-things-every-kafka-developer-should-know/#tip-5-power-of-record-headers
 
-            // this consumer receives http streaming events from the mbta api and sends them
-            // as kafka messages to the topic			
-            Consumer<ServerSentEvent<String>> httpConsumer = new Consumer<ServerSentEvent<String>>() {
-                public void accept(ServerSentEvent<String> content) {
+        // this consumer receives http streaming events from the mbta api and sends them
+        // as kafka messages to the topic			
+        Consumer < ServerSentEvent < String >> httpConsumer = new Consumer < ServerSentEvent < String >> () {
+            public void accept(ServerSentEvent < String > content) {
 
-					logger.info("Time: {} - event: name[{}], id [{}], content[{}] ",
-					LocalTime.now(), content.event(), content.id(), content.data());
+                logger.info("Time: {} - event: name[{}], id [{}], content[{}] ",
+                    LocalTime.now(), content.event(), content.id(), content.data());
 
-					ProducerRecord<String,String> record = 
-						new ProducerRecord<>(mbtaTopicName, content.data() );
+                ProducerRecord < String, String > record =
+                    new ProducerRecord < > (mbtaTopicName, content.data());
 
-					// add the header of the record event type (ie. Update, Delete etc.)
-					record.headers().add("event",content.event().getBytes(StandardCharsets.UTF_8));
+                // add the header of the record event type (ie. Update, Delete etc.)
+                record.headers().add("event", content.event().getBytes(StandardCharsets.UTF_8));
 
-					// send to kafka topic
-					template.send(record);
-                }
-            };
+                // send to kafka topic
+                template.send(record);
+            }
+        };
 
-            // this consumer is used to add http headers to the api requrest
-			Consumer headersConsumer = new Consumer<HttpHeaders>() {
-				@Override
-				public void accept(HttpHeaders httpHeaders) {
-					httpHeaders.add("accept","text/event-stream");
-					httpHeaders.add("x-api-key", mbtaApiKey);
-				}
-		  	};
+        // this consumer is used to add http headers to the api requrest
+        Consumer headersConsumer = new Consumer < HttpHeaders > () {
+            @Override
+            public void accept(HttpHeaders httpHeaders) {
+                httpHeaders.add("accept", "text/event-stream");
+                httpHeaders.add("x-api-key", mbtaApiKey);
+            }
+        };
 
-			/**
-			 * curl -sN -H "accept: text/event-stream" -H "x-api-key: 948fdf59bd114be9b9af1bcfea2d93ad" "https://api-v3.mbta.com/vehicles"
-			 * create a web client to the mbta api and add the necessary headers (api key & accept)
-			 * this code is based on the example from this article
-			 * https://www.baeldung.com/spring-server-sent-events
-			 */
-			WebClient client = WebClient.builder()
-                .baseUrl(mbtaApiEndpoint)
-                .defaultHeaders(headersConsumer)
-                .build();
-			
-			ParameterizedTypeReference<ServerSentEvent<String>> type
-			    = new ParameterizedTypeReference<ServerSentEvent<String>>() {};
+        /**
+         * curl -sN -H "accept: text/event-stream" -H "x-api-key: 948fdf59bd114be9b9af1bcfea2d93ad" "https://api-v3.mbta.com/vehicles"
+         * create a web client to the mbta api and add the necessary headers (api key & accept)
+         * this code is based on the example from this article
+         * https://www.baeldung.com/spring-server-sent-events
+         */
+        WebClient client = WebClient.builder()
+            .baseUrl(mbtaApiEndpoint)
+            .defaultHeaders(headersConsumer)
+            .build();
 
-			Flux<ServerSentEvent<String>> eventStream = client.get()
-			  .uri(mbtaApiUri)
-			  .retrieve()
-			  .bodyToFlux(type);
+        ParameterizedTypeReference < ServerSentEvent < String >> type = new ParameterizedTypeReference < ServerSentEvent < String >> () {};
 
-            eventStream.subscribe(
-                httpConsumer,
-                error -> logger.error("Error receiving SSE: {}", error),
-                () -> logger.info("Completed!!!")
-            );
-		
-			  // example
-			  // 2022-04-23 11:48:45.095  INFO 39107 --- [ctor-http-nio-3] o.c.M.MbtaStreamingClientApplication     : 
-			  // Time: 11:48:45.095005 - event: name[update], id [null], 
-			  // content[
-			  // {"attributes":{"bearing":45,"current_status":"INCOMING_AT","current_stop_sequence":4,
-			  // "direction_id":1,"label":"3087","latitude":42.2687,"longitude":-71.07989,
-			  // "occupancy_status":null,"speed":11.4,"updated_at":"2022-04-23T11:48:40-04:00"},
-			  // "id":"G-10046","links":{"self":"/vehicles/G-10046"},
-			  // "relationships":{"route":{"data":{"id":"Mattapan","type":"route"}},
-			  // "stop":{"data":{"id":"70270","type":"stop"}},
-			  // "trip":{"data":{"id":"50933526","type":"trip"}}},"type":"vehicle"}
-			  //]
+        Flux < ServerSentEvent < String >> eventStream = client.get()
+            .uri(mbtaApiUri)
+            .retrieve()
+            .bodyToFlux(type);
+
+        eventStream.subscribe(
+            httpConsumer,
+            error -> logger.error("Error receiving SSE: {}", error),
+            () -> logger.info("Completed!!!")
+        );
+
+        // example
+        // 2022-04-23 11:48:45.095  INFO 39107 --- [ctor-http-nio-3] o.c.M.MbtaStreamingClientApplication     : 
+        // Time: 11:48:45.095005 - event: name[update], id [null], 
+        // content[
+        // {"attributes":{"bearing":45,"current_status":"INCOMING_AT","current_stop_sequence":4,
+        // "direction_id":1,"label":"3087","latitude":42.2687,"longitude":-71.07989,
+        // "occupancy_status":null,"speed":11.4,"updated_at":"2022-04-23T11:48:40-04:00"},
+        // "id":"G-10046","links":{"self":"/vehicles/G-10046"},
+        // "relationships":{"route":{"data":{"id":"Mattapan","type":"route"}},
+        // "stop":{"data":{"id":"70270","type":"stop"}},
+        // "trip":{"data":{"id":"50933526","type":"trip"}}},"type":"vehicle"}
+        //]
     }
-    
+
 }
